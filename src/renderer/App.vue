e50bc79299d2d8686d2f3412c7434943b36e2b60
Seth Barton <sethwbarton@gmail.com>
2020-01-10 21:58:39 -0700

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'streamer',
+    data: {
+      video: 'https://www.w3schools.com/tags/movie.mp4'
+    },
+    methods: {
+      changeVideo () {
+        this.video = 'http://techslides.com/demos/sample-videos/small.mp4'
+      }
+    }
+  }
+
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ src/renderer/components/VideoViewer.vue
@@ -0,0 +1,16 @@
+<template>
+    <video width="320" height="240" controls>
+        <source  v-bind:src="video" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+</template>
+
+<script>
+    export default {
+        name: 'VideoViewer'
+    }
+</script>
+
+<style scoped>
+
+</style>
